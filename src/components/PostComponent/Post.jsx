be4d4f94c8aec7da7090defe1184161f27c6f8d3
RISHABH SHAH <rnshah1997@gmail.com>
2025-08-24 00:34:14 -0400

    update

--- src/components/PostComponent/Post.jsx
+++ src/components/PostComponent/Post.jsx
@@ -1,9 +1,12 @@
 import classes from "./Post.module.css";
 
+const names = ["Rishabh", "Sukriti"];
 const Post = () => {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <div className={classes.post}>
-      <p className={classes.author}>Rishabh</p>
+      <p className={classes.author}>{chosenName}</p>
       <p className={classes.text}>React.js !!</p>
     </div>
   );
